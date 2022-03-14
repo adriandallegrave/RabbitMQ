a7dc0f69ac9e57242a94b76d39798cab2c5439e7
adriandallegrave <adriandallegrave@gmail.com>
2022-03-14 16:48:39 -0300

    feat: consuming from a rabbitmq exchange

--- ExploreCalifornia.EmailService/Program.cs
+++ ExploreCalifornia.EmailService/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Run this console application and make a booking again
+// The message will appear in the console
+// If this console app is closed, the message will appear when it opens again
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("emailServiceQueue", true, false, false);
+channel.QueueBind("emailServiceQueue", "webappExchange", "");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var msg = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine(msg);
+};
+
+channel.BasicConsume("emailServiceQueue", true, consumer);
+
+Console.ReadLine();
+
+channel.Close();
+connection.Close();
