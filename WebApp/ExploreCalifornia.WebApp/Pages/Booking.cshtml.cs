caebfe341bb606b6e59da0a2e435bd28c62931b9
adriandallegrave <adriandallegrave@gmail.com>
2022-03-16 11:20:14 -0300

    chore: code cleanup on whole project

--- WebApp/ExploreCalifornia.WebApp/Pages/Booking.cshtml.cs
+++ WebApp/ExploreCalifornia.WebApp/Pages/Booking.cshtml.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ExploreCalifornia.WebApp.Pages
 {
     public class BookingModel : PageModel
     {
         public string TourName { get; set; }
-        
+
         public void OnGet()
         {
             TourName = Request.Query["tourname"];
